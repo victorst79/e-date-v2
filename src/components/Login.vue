74a823da530cc94038f7f928523fef51a731fa16
Victor Santaella <victorst79@gmail.com>
2019-11-12 23:16:02 +0100

    login with inputs done

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,20 @@
 <template>
-  <v-container>
+  <v-container id="login-c">
     <v-row>
-      <v-col cols="6">
+      <v-col cols="12" md="6" offset-md="3">
         <v-card>
-          <h3 class="titulo">Login</h3>
+          <h3 class="titulo text-center">Iniciar sesión</h3>
+          <v-text-field
+            v-model="user"
+            label="Usuario"
+            outlined
+            autofocus
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Contraseña"
+            outlined
+          ></v-text-field>
         </v-card>
       </v-col>
     </v-row>
@@ -15,8 +26,26 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Login',
-  data: () => ({
-
-  }),
+  data() {
+    return {
+        validation: '',
+        user: '',
+        password: ''
+    }
+  },
 });
 </script>
+
+<style lang="scss">
+#login-c {
+
+  h3 {
+    padding: 15px 0;
+  }
+
+  div.v-input {
+    width: 80%;
+    margin: 0 auto;
+  }
+}
+</style>
