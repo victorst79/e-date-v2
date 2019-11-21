8eb9bbbcfca540a398cf369ae52b17549a2e32ab
Victor Santaella <victorst79@gmail.com>
2019-11-21 22:08:23 +0100

    api.service done

--- src/common/api.service.ts
+++ src/common/api.service.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import { API_URL} from '@/common/config';
+import { API_URL } from '@/common/config';
 
 const ApiService = {
 
@@ -17,4 +17,21 @@ const ApiService = {
       });
   },
 
+  post(resource: string, params: any): any {
+    return Vue.axios.post(`${resource}`, params);
+  },
+
+  update(resource: string, slug: string ,params: any): any {
+    return Vue.axios.put(`${resource}/${slug}`, params);
+  },
+
+  delete(resource: string): any {
+    return Vue.axios.delete(resource)
+      .catch(error => {
+        throw new Error(`Error ApiService 001: ${error}`);
+      });
+  },
+
 };
+
+export default ApiService;
